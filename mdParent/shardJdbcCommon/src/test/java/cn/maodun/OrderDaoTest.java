676a5d4e298eb8711797b140ac023c8e9e63e5d1
xulei <xulei@shanhs.com>
2023-04-19 17:00:50 +0800

    分库分表：广播表 水平分库

--- mdParent/shardJdbcCommon/src/test/java/cn/maodun/OrderDaoTest.java
+++ mdParent/shardJdbcCommon/src/test/java/cn/maodun/OrderDaoTest.java
@@ -1,6 +1,8 @@
 package cn.maodun;
 
+import cn.maodun.dao.DictDao;
 import cn.maodun.dao.OrderDao;
+import cn.maodun.dao.UserDao;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,12 @@ public class OrderDaoTest {
     @Autowired
     OrderDao orderDao;
 
+    @Autowired
+    UserDao userDao;
+
+    @Autowired
+    DictDao dictDao;
+
     @Test
     public void testInsertOrder() {
         for (int i = 1; i < 20; i++) {
@@ -38,5 +46,52 @@ public class OrderDaoTest {
 
         List<Map> maps = orderDao.selectOrderbyIds(ids);
         System.out.println(maps);
+    }
+
+    @Test
+    public void testSelectOrderbyUserAndIds(){
+        List<Long> ids = new ArrayList<>();
+        ids.add(374121806463762432L);
+//        ids.add(373897037306920961L);
+
+//        List<Map> maps = orderDao.selectOrderbyUserAndIds(4L,ids);
+//        System.out.println(maps);
+    }
+
+    @Test
+    public void testInsertUser(){
+        for (int i = 10 ; i<14; i++){
+            Long id = i + 1L;
+            userDao.insertUser(id,"姓名"+ id );
+        }
+
+    }
+    @Test
+    public void testSelectUserbyIds(){
+        List<Long> userIds = new ArrayList<>();
+        userIds.add(1L);
+        userIds.add(2L);
+        List<Map> users = userDao.selectUserbyIds(userIds);
+        System.out.println(users);
+    }
+
+    @Test
+    public void testSelectUserInfobyIds(){
+        List<Long> userIds = new ArrayList<>();
+        userIds.add(1L);
+        userIds.add(2L);
+        List<Map> users = userDao.selectUserInfobyIds(userIds);
+        System.out.println(users);
+    }
+
+    @Test
+    public void testInsertDict(){
+        dictDao.insertDict(3L,"user_type","2","超级管理员");
+        dictDao.insertDict(4L,"user_type","3","二级管理员");
+    }
+    @Test
+    public void testDeleteDict(){
+        dictDao.deleteDict(3L);
+        dictDao.deleteDict(4L);
     }
 }
