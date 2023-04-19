e85739bda10260638fd66fe0be1fa4fc523cfaa8
xulei <xulei@shanhs.com>
2023-04-19 15:25:15 +0800

    分库分表：

---
+++ mdParent/shardJdbcCommon/src/main/java/cn/maodun/ShardJdbcCommonApplication.java
@@ -0,0 +1,15 @@
+package cn.maodun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardJdbcCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardJdbcCommonApplication.class, args);
+    }
+
+}
+
+
